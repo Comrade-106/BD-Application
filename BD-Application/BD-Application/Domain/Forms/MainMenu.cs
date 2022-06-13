885dd82bb7544883d40111978755aa4b99c9bd15
Ihor <vandaihor@gmail.com>
2022-06-13 18:10:23 +0300

    3

---
+++ BD-Application/BD-Application/Domain/Forms/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BD_Application.Domain.Forms {
+    public partial class MainMenu : Form {
+
+        public MainMenu() {
+            InitializeComponent();
+        }
+
+
+        private void MainMenu_Load(object sender, EventArgs e) {
+
+        }
+
+        private void AddButton_Click(object sender, EventArgs e) {
+            if (comboBox1.SelectedItem != null) {
+                switch (comboBox1.SelectedValue.ToString()) {
+                    case "Player":
+                        AddPlayerForm form = new AddPlayerForm();
+                        form.ShowDialog();
+                        break;
+                    case "Coach":
+                        AddCoachForm form = new AddCoachFormForm();
+                        form.ShowDialog();
+                        break;
+                    case "Team":
+                        AddTeamForm form = new AddTeamForm();
+                        form.ShowDialog();
+                        break;
+                    case "Tournament":
+                        AddTournamentForm form = new AddTournamentForm();
+                        form.ShowDialog();
+                        break;
+                        default: return;
+                }
+            }
+        }
+
+        private void ChangeButton_Click(object sender, EventArgs e) {
+
+        }
+
+        private void ViewButton_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
--- BD-Application/BD-Application/Domain/Forms/TournamentForms/AddTournamentForm.cs
+++ BD-Application/BD-Application/Domain/Forms/TournamentForms/AddTournamentForm.cs
@@ -1,17 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BD_Application.Domain.Forms.TournamentForms {
     public partial class AddTournamentForm : Form {
+        private readonly List<Organizer> organizers;
+        private Tournament currentTournament = null;
+
         public AddTournamentForm() {
             InitializeComponent();
+            if ((organizers = GetAllOrganizers()) == null) {
+                MessageBox.Show("Can`t get info about organizer from DB", "Error!");
+                return;
+            }
+            FillOrganizersBox();
+        }
+
+        private List<Organizer> GetAllOrganizers() {
+            List<Organizer> organizers = new List<Organizer>();
+
+            if (false) {//get organizers from db
+                return null;
+            }
+
+            return organizers;
+        }
+
+        private void FillOrganizersBox() {
+            OrganizerBox.Items.Clear();
+            OrganizerBox.DataSource = organizers;
+            OrganizerBox.DisplayMember = "name";
+            OrganizerBox.ValueMember = "id";
+        }
+
+        private void AddTournamentButton_Click(object sender, EventArgs e) {
+            if (NameBox.Text != String.Empty && OrganizerBox.SelectedItem != null &&
+                    DateStartBox.Value != null && DateEndBox.Value != null && PrizePoolBox.Text != String.Empty) {
+                if (double.TryParse(PrizePoolBox.Text, out double prize)) {
+                    if (prize >= 0.0) {
+                        currentTournament.Name = NameBox.Text;
+                        currentTournament.Organizer = organizers.Find(x => x.Id == Convert.ToInt32(OrganizerBox.SelectedValue));
+                        currentTournament.DateStart = DateStartBox.Value;
+                        currentTournament.DateEnd = DateEndBox.Value;
+                        currentTournament.PrizePool = prize;
+                    } else {
+                        MessageBox.Show("Prize pool can`t be less than 0", "Message!");
+                    }
+                } else {
+                    MessageBox.Show("Prize pool is a number with comma", "Message!");
+                }
+            } else {
+                MessageBox.Show("You entered not all info ", "Message!");
+            }
         }
     }
 }
